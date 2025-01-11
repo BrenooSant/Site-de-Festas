706f3af8f6549586c651502c5ed204328154d1c7
Breno Santos <brenoosant19@gmail.com>
2025-01-11 20:09:57 -0300

    Update main.js
    criando fechamento dos botões para responsividade

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript carregado!');
+
+    const menuToggle = document.getElementById('menu-toggle');
+    const buttonContainer = document.getElementById('button-container');
+  
+    if (menuToggle && buttonContainer) {
+      menuToggle.addEventListener('click', () => {
+        buttonContainer.classList.toggle('show');
+        console.log('Menu toggle clicado!');
+      });
+    } else {
+      console.error('Elementos menuToggle ou buttonContainer não encontrados!');
+    }
+  });
+  
+
 let count = 1
 document.getElementById("radio1").checked = true;
 
